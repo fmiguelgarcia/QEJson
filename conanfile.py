e8a3ad73ad24aa57feebbb87918eeb31cc51bd6d
fmiguelgarcia <francisco.miguel.garcia.rodriguez@gmail.com>
2017-02-14 10:40:58 +0100

    Fixing conan package recipe

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,4 @@
 from conans import ConanFile, CMake 
-from conans.tools import os_info
 import multiprocessing
 
 class QEJsonS11nConan(ConanFile):
@@ -9,16 +8,17 @@ class QEJsonS11nConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     license = "https://www.gnu.org/licenses/lgpl-3.0-standalone.html"
     generators = "cmake"
+    url = "ssh://miguel@migroslinx.no-ip.org/home/miguel/git/QE/QEJsonS11n"
+    description = "JSon Serialization library based on Annotations"
 
     def source(self):
-        self.run("git clone ssh://miguel@migroslinx.no-ip.org/home/miguel/git/QE/QEJsonS11n") 
+        self.run("git clone %s" % self.url) 
 
     def build(self):
         cmake = CMake( self.settings)
         self.run( "conan install %s/QEJsonS11n" % self.conanfile_directory)
         self.run( "cmake %s/QEJsonS11n %s" % (self.conanfile_directory, cmake.command_line))
-        self.run( "cmake --build . %s %s" % (cmake.build_config, 
-            ("-- -j %d " % multiprocessing.cpu_count()) if os_info.is_linux else ""))
+        self.run( "cmake --build . %s"  % cmake.build_config ) 
 
     def package(self):
         self.copy( pattern="*.hpp", dst="include/QEJsonS11n/", src="QEJsonS11n/src")
